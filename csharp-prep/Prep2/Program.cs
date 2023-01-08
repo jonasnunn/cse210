de78740c75ca6752effea77f54f77693c74c2ee1
Jonas_Nunn <jonasnunn@gmail.com>
2023-01-07 22:02:25 -0700

    C# tasks 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradeStr = Console.ReadLine();
+
+        int grade = int.Parse(gradeStr);
+
+        string letter = ""; 
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got an/a {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Good job. You passed the class!");
+        }
+
+        else
+            Console.WriteLine("You didn't pass, but you will next time!");
     }
 }

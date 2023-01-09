588ee9697ca3ad5f2940a1d6dbce9d7a752cabfa
Jonas_Nunn <jonasnunn@gmail.com>
2023-01-09 10:34:11 -0700

    task 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        // Console.WriteLine($"Your random number is {number}");
+
+        bool guessed = false;
+        int counter = 0;
+
+        while(guessed == false){
+        Console.Write("What is your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+        Console.WriteLine($"They guessed {guess}");
+        counter += 1;
+
+        if (guess > number)
+        {
+            Console.WriteLine("Your guess is high");
+        } 
+        else if (guess < number)
+        {
+            Console.WriteLine("Your guess is low");
+        }
+        else
+        {
+            Console.WriteLine("Correct. Perfect. Great job!");
+            guessed = true;
+        }
+        }
+        Console.WriteLine($"It took {counter} guesses to guess the number");
     }
 }
